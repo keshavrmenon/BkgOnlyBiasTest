d0bb47723081d578f28b3de9290e0fc1978ffa03
keshavrmenon <keshavmenon@iisc.ac.in>
2021-10-07 15:50:18 +0530

    nmssm

--- runBias_.sh
+++ runBias_.sh
@@ -6,9 +6,10 @@ massX=$2
 icat=$3
 
 truth=$4
-date="18_01_2021"
-datacard_folder="/afs/cern.ch/work/k/kmenon/CMSSW_10_2_13/src/BkgOnlyBiasTest"
-datacard=cms_HHbbgg_datacard_${sig}${massX}_${date}_2016_2017_2018
+massY=$7
+date="27_12_2020"
+datacard_folder="/afs/cern.ch/work/k/kmenon/CMSSW_10_2_13/src/BkgOnlyBiasTest/NMSSM"
+datacard=cms_HHbbgg_datacard_${sig}_X${massX}_Y${massY}_${date}_2016_2017_2018
 NGEN=1000
 mu_inj=0.0
 echo $mu_inj
@@ -18,25 +19,26 @@ seed0=$5
 seed1=$6
 
 
-    CAT=DoubleHTag_${icat}_13TeV
-    cd /afs/cern.ch/work/k/kmenon/CMSSW_10_2_13/src
-    eval `scramv1 runtime -sh`
-    cd $datacard_folder
+CAT=DoubleHTag_${icat}_13TeV
+cd /afs/cern.ch/work/k/kmenon/CMSSW_10_2_13/src
+eval `scramv1 runtime -sh`
+cd $datacard_folder
     
-    pdfidx=pdfindex_$CAT
-    SETUP=${CAT}.${sig}_X${massX}_${date}.t$truth.GENSEED${seed0}
-    GENERATE=higgsCombine${SETUP}.GenerateOnly.mH125.${seed0}.root
-    TOYS=higgsCombineTest.GenerateOnly.${SETUP}.${seed0}.root
-    echo $SETUP
+pdfidx=pdfindex_$CAT
+SETUP=${CAT}.${sig}_X${massX}_Y${massY}_${date}.t$truth.GENSEED${seed0}
+GENERATE=higgsCombine${SETUP}.GenerateOnly.mH125.${seed0}.root
+TOYS=higgsCombineTest.GenerateOnly.${SETUP}.${seed0}.root
+echo $SETUP
     
-    GENERATING1="combine ${datacard}_${CAT}_wIntegral.root -M GenerateOnly -m 125 --setParameters $pdfidx=$truth --expectSignal $mu_inj -t $NGEN --saveToys -n ${SETUP} --freezeParameters $pdfidx,rgx{.*} -s ${seed0} --toysFrequentist"
-    echo $GENERATING1
-    eval $GENERATING1
+GENERATING1="combine ${datacard}_${CAT}_wIntegral.root -M GenerateOnly -m 125 --setParameters $pdfidx=$truth --expectSignal 0 -t $NGEN --saveToys -n ${SETUP} --freezeParameters $pdfidx,rgx{.*} -s ${seed0}"
+echo $GENERATING1
+eval $GENERATING1
 
-    mv $GENERATE $TOYS
-    echo "generated " $TOYS
-    MLFITTING1="combineTool.py ${datacard}_${CAT}_wIntegral.root -M MultiDimFit -m 125 --expectSignal $mu_inj --toysFile $TOYS --cminDefaultMinimizerStrategy 0 -n ${SETUP}_envelop -t $NGEN --saveFitResult --saveSpecifiedIndex $pdfidx -P r --setParameterRanges r=-5,5 --freezeParameters allConstrainedNuisances -s ${seed1} --job-mode condor --task-name condor-${SETUP} --sub-opts='+JobFlavour="tomorrow"' --job-dir $datacard_folder X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 shapeBkg_bkg_mass_ch1_${CAT}_integral_extended,shapeBkg_bkg_mass_ch1_${CAT}_integral"
-    echo ${MLFITTING1}
-    combineTool.py ${datacard}_${CAT}_wIntegral.root -M MultiDimFit -m 125 --expectSignal 0 --toysFile ${TOYS} --cminDefaultMinimizerStrategy 0 -n ${SETUP}_envelop -t ${NGEN} --saveSpecifiedIndex $pdfidx -P shapeBkg_bkg_mass_ch1_${CAT}__norm --freezeParameters r,rgx{.*},MH --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --saveSpecifiedFunc shapeBkg_bkg_mass_ch1_${CAT}_integral_extended,shapeBkg_bkg_mass_ch1_${CAT}_integral -s ${seed1} --job-mode condor --task-name condor-${SETUP} --sub-opts='+JobFlavour="tomorrow"' --job-dir ${datacard_folder}
-    #rm -f $TOYS
-    echo "fitted $pdfidx toy with envelop"
+mv $GENERATE $TOYS
+echo "generated " $TOYS
+MLFITTING1="combineTool.py ${datacard}_${CAT}_wIntegral.root -M MultiDimFit -m 125 --expectSignal $mu_inj --toysFile $TOYS --cminDefaultMinimizerStrategy 0 -n ${SETUP}_envelop -t $NGEN --saveFitResult --saveSpecifiedIndex $pdfidx -P r --setParameterRanges r=-5,5 --freezeParameters allConstrainedNuisances -s ${seed1} --job-mode condor --task-name condor-${SETUP} --sub-opts='+JobFlavour="microcentury"' --job-dir $datacard_folder X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 shapeBkg_bkg_mass_ch1_${CAT}_integral_extended,shapeBkg_bkg_mass_ch1_${CAT}_integral"
+echo ${MLFITTING1}
+combineTool.py ${datacard}_${CAT}_wIntegral.root -M MultiDimFit -m 125 --expectSignal 0 --toysFile ${TOYS} --cminDefaultMinimizerStrategy 0 -n ${SETUP}_envelop -t ${NGEN} --saveSpecifiedIndex $pdfidx -P shapeBkg_bkg_mass_ch1_${CAT}__norm --freezeParameters r,rgx{.*},MH --X-rtd MINIMIZER_freezeDisassociatedParams --X-rtd MINIMIZER_multiMin_hideConstants --X-rtd MINIMIZER_multiMin_maskConstraints --X-rtd MINIMIZER_multiMin_maskChannels=2 --saveSpecifiedFunc shapeBkg_bkg_mass_ch1_${CAT}_integral_extended,shapeBkg_bkg_mass_ch1_${CAT}_integral -s ${seed1} --job-mode condor --task-name condor-${SETUP} --sub-opts='+JobFlavour="microcentury"' --job-dir ${datacard_folder}
+	
+#rm -f $TOYS
+echo "fitted $pdfidx toy with envelop"
